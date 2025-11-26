6ddc53909ec2894a9fee8ee5f7fb12604d34042e
icysaif7 <saifullahsyed11@gmail.com>
2025-11-26 21:26:41 +0530

    Add Python app and Jenkinsfile

---
+++ app.py
@@ -0,0 +1,14 @@
+cat > app.py <<'EOF'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Jenkins Multi-Stage Pipeline!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+EOF
+
+
